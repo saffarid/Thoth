b163f44784443e89e20802c9279cac7e67ef2856
TheMachine <pavlov.nikita@mail.ru>
2021-10-05 06:50:20 +0500

    Начало структуры Thoth Lite. Добавлены таблицы типа GUIDE

--- plugins/core/src/main/java/ThothCore/DBLiteStructure/DBLiteStructure.java
+++ plugins/core/src/main/java/ThothCore/DBLiteStructure/DBLiteStructure.java
@@ -1,13 +1,128 @@
 package ThothCore.DBLiteStructure;
 
 import Database.DataBaseSQL;
+import Database.Table;
+import Database.TableColumn;
 
 public class DBLiteStructure extends DataBaseSQL {
 
+    public final String TABLE = "table";
+    public final String PROJECT_TABLE = "project_table";
+    public final String GUIDE = "guide";
+    public final String CONSTANTS = "constants";
+    public final String SYSTEM_TABLE = "system_table";
+    public final String SYSTEM_GUIDE = "system_guide";
+    public final String SYSTEM_CONSTANTS = "system_constants";
 
     public DBLiteStructure() {
+        super();
+
+        //Таблицы типа GUIDE
+        tables.add(new CountTypes());
+        tables.add(new ProductTypes());
+        tables.add(new OrderStatus());
+        tables.add(new Partners());
+        tables.add(new IncomeTypes());
 
     }
 
+    /**
+     * Таблица едениц измерения
+     */
+    class CountTypes extends Table{
+        public static final String TABLE_NAME = "count_type";
+        public static final String COUNT_TYPE = "count_type";
+        public CountTypes() {
+            super();
+            name = TABLE_NAME;
+            type = GUIDE;
+
+            addColumn(new TableColumn(
+                    COUNT_TYPE, "varchar(10)", true, true
+            ));
+
+        }
+    }
+
+    /**
+     * Таблица типов продуктов
+     */
+    class ProductTypes extends Table{
+        public static final String TABLE_NAME = "product_types";
+        public static final String PRODUCT_TYPES = "product_types";
+
+        public ProductTypes() {
+            super();
+            name = TABLE_NAME;
+            type = GUIDE;
+
+            addColumn(new TableColumn(
+                    PRODUCT_TYPES, "varchar(255)", true, true
+            ));
+        }
+    }
+
+    /**
+     * Таблица статусов заказа
+     */
+    class OrderStatus extends Table{
+        public static final String TABLE_NAME = "order_status";
+        public static final String ORDER_STATUS = "order_status";
+
+        public OrderStatus() {
+            super();
+            name = TABLE_NAME;
+            type = GUIDE;
+
+            addColumn(new TableColumn(
+                    ORDER_STATUS, "varchar(255)", true, true
+            ));
+        }
+    }
+
+    /**
+     * Таблица партнеров
+     */
+    class Partners extends Table{
+        public static final String TABLE_NAME = "partners";
+        public static final String NAME = "name";
+        public static final String PHONE = "phone";
+        public static final String WEB = "web";
+
+        public Partners() {
+            super();
+            name = TABLE_NAME;
+            type = GUIDE;
+
+            addColumn(new TableColumn(
+                    NAME, "text", true, true
+            ));
+            addColumn(new TableColumn(
+                    PHONE, "varchar(20)", false, false
+            ));
+            addColumn(new TableColumn(
+                    WEB, "varchar(255)", false, false
+            ));
+        }
+    }
+
+    /**
+     * Таблица категорий доходов
+     */
+    class IncomeTypes extends Table{
+
+        public static final String TABLE_NAME = "income_types";
+        public static final String INCOME_TYPE = "income_type";
+
+        public IncomeTypes() {
+            super();
+            name = TABLE_NAME;
+            type = GUIDE;
+
+            addColumn(new TableColumn(
+                    INCOME_TYPE, "varchar(255)", true, true
+            ));
+        }
+    }
 
 }
